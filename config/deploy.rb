6fb7914a0c09159399ce85f00073692c4cd32a8a
Andrey Esaulov <esaulov.andrey@gmail.com>
2012-07-26 16:15:12 +0200

    trying to make deployment script work

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "blog"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, false
+set :use_sudo, true
 
 set :scm, "git"
 set :repository, "git@github.com:andremaha/#{application}.git"
